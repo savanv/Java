ae75adf0718864bd4bb7312c05ded01fd70bd34d
savanv <31373390+savanv@users.noreply.github.com>
2020-12-17 13:55:28 +0530

    Add files via upload

---
+++ svyas/src/main/java/com/savan/svyas/SvyasApplication.java
@@ -0,0 +1,13 @@
+package com.savan.svyas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvyasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvyasApplication.class, args);
+	}
+
+}
---
+++ svyas/src/test/java/com/savan/svyas/SvyasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savan.svyas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvyasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
